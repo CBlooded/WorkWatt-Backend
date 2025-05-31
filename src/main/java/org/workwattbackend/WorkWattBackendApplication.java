b4fac491ca97dc65df7464a1a536be6ac7baf328
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-05-31 13:34:05 +0200

    initial

---
+++ src/main/java/org/workwattbackend/WorkWattBackendApplication.java
@@ -0,0 +1,13 @@
+package org.workwattbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWattBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkWattBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/workwattbackend/WorkWattBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workwattbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWattBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
