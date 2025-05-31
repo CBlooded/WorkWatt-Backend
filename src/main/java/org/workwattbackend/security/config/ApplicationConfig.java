4dcb8b938be05ca2a121aa8fed9361a3ea6db74e
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-05-31 18:42:51 +0200

    for merge

--- src/main/java/org/workwattbackend/security/config/ApplicationConfig.java
+++ src/main/java/org/workwattbackend/security/config/ApplicationConfig.java
@@ -1,26 +1,44 @@
-//package org.workwattbackend.security.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.workwattbackend.user.UserRepository;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class ApplicationConfig {
-//    private final UserRepository userRepository;
-//
-//    //TODO userDetails, Authentication Manager, authentication provider
-////    @Bean
-////    public UserDetailsService userDetails() {
-////        return id -> userRepository.findById(id).get();
-////    }
-//
-//    @Bean
-//    public PasswordEncoder encoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package org.workwattbackend.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.workwattbackend.user.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetails() {
+        return email -> (UserDetails) userRepository.findByEmail(email).orElseThrow();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetails());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
