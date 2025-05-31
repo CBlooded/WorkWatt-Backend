c42005bd3e22ac7ff81863e22e5fd312e3adb850
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2025-05-31 19:25:37 +0200

    Configure mailing service

---
+++ src/main/java/org/workwattbackend/mailing/model/EmailController.java
@@ -0,0 +1,31 @@
+package org.workwattbackend.mailing.model;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.workwattbackend.mailing.EmailService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/mailing")
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService service;
+
+    @GetMapping
+    public ResponseEntity<Void> sendTestMail() throws MessagingException {
+        service.sendMail(
+            EmailData
+                .builder()
+                .to("mikiflor24@gmail.com")
+                .subject("test")
+                .htmlBody("<h1>Hello</h1>")
+                .build()
+        );
+        return ResponseEntity.ok().build();
+    }
+}
