fa913a93a83f91247912779ecbda92027050cdf5
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-05-31 18:16:56 +0200

    endpoint for desktop ap

--- src/main/java/org/workwattbackend/security/config/ApplicationConfig.java
+++ src/main/java/org/workwattbackend/security/config/ApplicationConfig.java
@@ -1,26 +1,26 @@
-package org.workwattbackend.security.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.workwattbackend.user.UserRepository;
-
-@Configuration
-@RequiredArgsConstructor
-public class ApplicationConfig {
-    private final UserRepository userRepository;
-
-    //TODO userDetails, Authentication Manager, authentication provider
+//package org.workwattbackend.security.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.workwattbackend.user.UserRepository;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class ApplicationConfig {
+//    private final UserRepository userRepository;
+//
+//    //TODO userDetails, Authentication Manager, authentication provider
+////    @Bean
+////    public UserDetailsService userDetails() {
+////        return id -> userRepository.findById(id).get();
+////    }
+//
 //    @Bean
-//    public UserDetailsService userDetails() {
-//        return id -> userRepository.findById(id).get();
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
 //    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-}
+//
+//}
