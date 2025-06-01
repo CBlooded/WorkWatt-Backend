9064d67763e5b5870908b157fd62dae1ca4858ef
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-06-01 11:47:53 +0200

    scheduler

--- src/main/java/org/workwattbackend/mailing/EmailService.java
+++ src/main/java/org/workwattbackend/mailing/EmailService.java
@@ -8,8 +8,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.workwattbackend.mailing.model.EmailData;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +37,28 @@ public class EmailService {
         helper.setText(emailData.getHtmlBody(), true);
 
         mailSender.send(helper.getMimeMessage());
+    }
+
+
+    public String loadHtmlTemplate(String path) {
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(path));
+            return new String(encoded, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Error in html file", e);
+        }
+    }
+
+
+    @Scheduled(cron = "0 50 11 * * *")
+    public void dayReport() throws MessagingException {
+        String htmlContent = loadHtmlTemplate("src/main/resources/templates/mail.html");
+
+        EmailData mail = EmailData.builder()
+                .to("royopat140@jeanssi.com")
+                .subject("Report " + LocalDateTime.now().toString())
+                .htmlBody(htmlContent).build();
+        sendMail(mail);
     }
 
 }
