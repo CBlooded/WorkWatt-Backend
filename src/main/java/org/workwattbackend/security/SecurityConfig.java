b9eacc6c7f0a8ebfc33d6658baf3b5936eb8bcf5
Patryk Mazurek <Patryk.m3456@gmail.com>
2025-06-01 04:46:14 +0200

    JWT Authentication

--- src/main/java/org/workwattbackend/security/SecurityConfig.java
+++ src/main/java/org/workwattbackend/security/SecurityConfig.java
@@ -1,31 +1,39 @@
 package org.workwattbackend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.workwattbackend.security.config.JwtAuthentication;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthentication jwtAuthentication;
     //TODO finish
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(request ->
-                request.requestMatchers("/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/api/v1/auth/authenticate")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
 
-            )
-            .sessionManagement(
-                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .build();
+                )
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthentication, UsernamePasswordAuthenticationFilter.class).build();
     }
 }
 
